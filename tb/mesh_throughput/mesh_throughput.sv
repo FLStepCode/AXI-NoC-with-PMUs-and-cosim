4808af0c734e25df443c8000c77f36f3cadbeb65
Elgrush <125126782+Elgrush@users.noreply.github.com>
2025-12-26 00:31:04 +0300

    test

--- tb/mesh_throughput/mesh_throughput.sv
+++ tb/mesh_throughput/mesh_throughput.sv
@@ -1,6 +1,6 @@
 `timescale 1ns/1ps
 
-module throughput_wrapper (
+module mesh_throughput (
     input aresetn,
 
     output logic awready[16],
--- tb/tb_uart/tb_uart.py
+++ tb/tb_uart/tb_uart.py
@@ -4,7 +4,7 @@ from cocotb.clock import Clock
 from cocotbext.uart import UartSource
 
 @cocotb.test
-async def test(dut):
+async def test_uart_rec(dut):
     cocotb.start_soon(Clock(dut.clk_i, 1, 'ns').start())
 
     source = UartSource(dut.rx_i, baud=100_000_000, bits=8)
@@ -30,3 +30,23 @@ async def test(dut):
 
     for _ in range(10):
         await RisingEdge(dut.clk_i)
+
+@cocotb.test
+async def test_uart_trans(dut):
+    cocotb.start_soon(Clock(dut.clk_i, 1, 'ns').start())
+
+    source = UartSink(dut.tx_o, baud=100_000_000, bits=8)
+
+    dut.arstn_i.value = 0
+    dut.data_valid_i.value = 0
+    await RisingEdge(dut.clk_i)
+    dut.arstn_i.value = 1
+
+    for i in range(256):
+        dut.data_valid_i.value = 1
+        dut.data_i.value = i
+        await RisingEdge(dut.clk_i)
+        dut.data_valid_i.value = 0
+
+        for j in range(100):
+            await RisingEdge(dut.clk_i)
