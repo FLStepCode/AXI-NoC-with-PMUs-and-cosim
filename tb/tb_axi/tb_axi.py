28da80e6762287283f63544df4cfe774728b2b58
Elgrush <125126782+Elgrush@users.noreply.github.com>
2026-01-19 16:09:20 +0300

    tb_axi_memory fixed

--- tb/tb_axi/tb_axi.py
+++ tb/tb_axi/tb_axi.py
@@ -2,14 +2,14 @@ import cocotb
 from cocotb.triggers import RisingEdge, Timer, First
 
 async def wait_to_finish(dut):
-    for _ in range(10000):
+    while True:
         await RisingEdge(dut.ACLK)
         if dut.finished.value: return
 
 @cocotb.test
 async def test(dut):
 
-    timeout = Timer(10_000_000, units='ns')
+    timeout = Timer(1_000, units='ns')
 
     result = await First(timeout, cocotb.start_soon(wait_to_finish(dut)))
     assert result is not timeout, "Design has hung!"
